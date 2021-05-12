91c5f3942d2344d7aeaaa60abeca4acb9f583daa
Eric Masi <eric.masi90@gmail.com>
2021-05-12 19:45:05 -0400

    Show error on http request fail

--- front-end/src/features/auth/Login.js
+++ front-end/src/features/auth/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import superagent from 'superagent';
@@ -9,36 +9,47 @@ export default function Login() {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
   const history = useHistory();
+  const [authError, setAuthError] = useState(false);
 
   const onSubmit = async (data) => {
-    const {
-      body: { accountStatus, authenticationResult },
-    } = await superagent
-      .post(
-        'https://z9zn346vaf.execute-api.us-east-1.amazonaws.com/dev/users/log-in'
-      )
-      .send(data);
+    try {
+      const {
+        body: { accountStatus, authenticationResult },
+      } = await superagent
+        .post(
+          'https://z9zn346vaf.execute-api.us-east-1.amazonaws.com/dev/users/log-in'
+        )
+        .send(data);
 
-    if (accountStatus === '') {
-      dispatch(updateTokens(authenticationResult));
-      history.push('/user');
+      if (accountStatus === '') {
+        dispatch(updateTokens(authenticationResult));
+        history.push('/user');
+      }
+    } catch (error) {
+      setAuthError(true);
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex-col flex-center m-center p-xy-2"
-    >
-      <label className="m-b-2">
-        Username: <input type="text" {...register('username')} />
-      </label>
+    <>
+      {authError ? (
+        <h1>There was an error 💩</h1>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex-col flex-center m-center p-xy-2"
+        >
+          <label className="m-b-2">
+            Username: <input type="text" {...register('username')} />
+          </label>
 
-      <label className="m-b-2">
-        Password: <input type="password" {...register('password')} />
-      </label>
+          <label className="m-b-2">
+            Password: <input type="password" {...register('password')} />
+          </label>
 
-      <button type="submit">Submit</button>
-    </form>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </>
   );
 }
